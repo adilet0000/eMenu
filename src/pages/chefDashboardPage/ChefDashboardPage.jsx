2aa655cfc1c91a355d0293cdc40ce9d39a78c8b4
adilet0000 <148931811+adilet0000@users.noreply.github.com>
2024-09-12 19:27:27 +0600

    Delete src directory

--- src/pages/chefDashboardPage/ChefDashboardPage.jsx
+++
@@ -1,8 +0,0 @@
-
-export function ChefDashboardPage() {
-   return (
-      <>
-
-      </>
-   )
-}
--- src/pages/orderConfirmationPage/OrderConfirmationPage.jsx
+++
@@ -1,8 +0,0 @@
-
-export function OrderConfirmationPage() {
-   return (
-      <>
-
-      </>
-   )
-}
